e9f5f1340304824cfbf447a90dbe6eb19dac4e4f
刘德财 <442823529@qq.com>
2024-07-08 21:09:24 +0800

    开发RBAC功能

--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -2,7 +2,10 @@
   <div>
     <BasicTable @register="registerTable" @fetch-success="onFetchSuccess">
       <template #toolbar>
-        <a-button type="primary" @click="handleCreate"> 新增菜单 </a-button>
+        <a-button type="primary" @click="handleCreate">{{ t('lang.button.create') }}</a-button>
+        <a-button type="primary" @click="toggleExpandCollapse">{{
+          isExpanded ? t('lang.button.collapse') : t('lang.button.expand')
+        }}</a-button>
       </template>
       <template #bodyCell="{ column, record }">
         <template v-if="column.key === 'action'">
@@ -37,14 +40,16 @@
   import { useDrawer } from '@/components/Drawer';
   import { useI18n } from '@/hooks/web/useI18n';
   import MenuDrawer from './editDrawer.vue';
-  import { nextTick } from 'vue';
+  import { nextTick, ref } from 'vue';
 
   defineOptions({ name: 'MenuManagement' });
 
   const { t } = useI18n();
+  const isExpanded = ref(false);
   const { notification } = useMessage();
+
   const [registerDrawer, { openDrawer }] = useDrawer();
-  const [registerTable, { reload, expandAll }] = useTable({
+  const [registerTable, { reload, expandAll, collapseAll }] = useTable({
     title: '菜单列表',
     api: queryMenuTree,
     beforeFetch: (params) => {
@@ -100,8 +105,18 @@
     reload();
   }
 
+  function toggleExpandCollapse() {
+    if (isExpanded.value) {
+      collapseAll();
+    } else {
+      expandAll();
+    }
+    isExpanded.value = !isExpanded.value;
+  }
+
   function onFetchSuccess() {
     // 演示默认展开所有表项
     nextTick(expandAll);
+    isExpanded.value = true;
   }
 </script>
